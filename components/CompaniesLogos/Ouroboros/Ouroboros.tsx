22b878f899604bd1d833c7f5eb66e39389dd7d3d
herlnd <hernanlandoni@gmail.com>
2022-10-01 00:36:05 -0300

    added neucom logo

---
+++ components/CompaniesLogos/Ouroboros/Ouroboros.tsx
@@ -0,0 +1,7 @@
+import styles from './Ouroboros.module.css';
+
+const Ouroboros = () => {
+  return <div className={styles.ouroboros}>Ouroboros</div>;
+};
+
+export default Ouroboros;
---
+++ components/CompaniesLogos/Upeo/Upeo.tsx
@@ -0,0 +1,6 @@
+import styles from './Upeo.module.css';
+const Upeo = () => {
+  return <div className={styles.upeo}>Upeo</div>;
+};
+
+export default Upeo;
